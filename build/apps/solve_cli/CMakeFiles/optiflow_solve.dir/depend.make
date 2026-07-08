82cf5aef32eeb1ae82f684230d76b12df1f37a44
Lorenzo Sanfilippo <lorenzo.sanfilippo@gmail.com>
2026-07-09 00:27:59 +0200

    Add gitignore and stop tracking build artifacts

--- build/apps/solve_cli/CMakeFiles/optiflow_solve.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for optiflow_solve.
-# This may be replaced when dependencies are built.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lorenzo/work/optiflow/tests
-# Build directory: /Users/lorenzo/work/optiflow/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[optiflow_tests]=] "/Users/lorenzo/work/optiflow/build/tests/optiflow_tests")
-set_tests_properties([=[optiflow_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/lorenzo/work/optiflow/tests/CMakeLists.txt;8;add_test;/Users/lorenzo/work/optiflow/tests/CMakeLists.txt;0;")
